0484598f3f49b9a323c837ab61bcbf1981946b6b
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2025-08-12 16:22:13 +0700

    membuat navigasi antar halaman (MVVM)

---
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -0,0 +1,49 @@
+package com.example.navigasiku
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigasiku.view.FormIsian
+import com.example.navigasiku.view.TampilData
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+@Composable
+fun DataApp (
+    navController: NavHostController = rememberNavController()
+){
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+
+            modifier = Modifier.padding(isiRuang)) {
+            composable(route = Navigasi.Formulir.name){
+                FormIsian (
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name){
+                TampilData (
+                    onBackBtnClick = {cancelAndBackToFormulir(navController)}
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+){
+    navController.popBackStack(Navigasi.Formulir.name, inclusive = false)
+}
